47c456aecdad93b8e5e206bd52b8c57dbd0fe7e8
faiqfarooq04 <faiqfarooq04@gmail.com>
2025-12-15 05:35:38 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../context/authContext'
+import { useAuth } from '../context/AuthContext'
 import { 
   Menu, X, Baby, Home, Info, Phone, LogIn, UserPlus, 
   LogOut, User, LayoutDashboard, Stethoscope 
